cd4bba4650c506cb55228e76418734a2b0e51528
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2022-11-14 10:48:17 +0000

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.resetbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn00.Location = new System.Drawing.Point(12, 12);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(120, 120);
+            this.btn00.TabIndex = 0;
+            this.btn00.Text = "?";
+            this.btn00.UseVisualStyleBackColor = true;
+            this.btn00.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn10.Location = new System.Drawing.Point(138, 12);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(120, 120);
+            this.btn10.TabIndex = 1;
+            this.btn10.Text = "?";
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn20
+            // 
+            this.btn20.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn20.Location = new System.Drawing.Point(264, 12);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(120, 120);
+            this.btn20.TabIndex = 2;
+            this.btn20.Text = "?";
+            this.btn20.UseVisualStyleBackColor = true;
+            this.btn20.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn21.Location = new System.Drawing.Point(264, 138);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(120, 120);
+            this.btn21.TabIndex = 3;
+            this.btn21.Text = "?";
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn11.Location = new System.Drawing.Point(138, 138);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(120, 120);
+            this.btn11.TabIndex = 4;
+            this.btn11.Text = "?";
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn01
+            // 
+            this.btn01.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn01.Location = new System.Drawing.Point(12, 138);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(120, 120);
+            this.btn01.TabIndex = 5;
+            this.btn01.Text = "?";
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn22.Location = new System.Drawing.Point(264, 264);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(120, 120);
+            this.btn22.TabIndex = 6;
+            this.btn22.Text = "?";
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn12.Location = new System.Drawing.Point(138, 264);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(120, 120);
+            this.btn12.TabIndex = 7;
+            this.btn12.Text = "?";
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn02
+            // 
+            this.btn02.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn02.Location = new System.Drawing.Point(12, 264);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(120, 120);
+            this.btn02.TabIndex = 8;
+            this.btn02.Text = "?";
+            this.btn02.UseVisualStyleBackColor = true;
+            this.btn02.Click += new System.EventHandler(this.button_Click);
+            // 
+            // resetbtn
+            // 
+            this.resetbtn.Cursor = System.Windows.Forms.Cursors.No;
+            this.resetbtn.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetbtn.Location = new System.Drawing.Point(264, 390);
+            this.resetbtn.Name = "resetbtn";
+            this.resetbtn.Size = new System.Drawing.Size(120, 32);
+            this.resetbtn.TabIndex = 9;
+            this.resetbtn.Text = "Reset";
+            this.resetbtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(398, 434);
+            this.Controls.Add(this.resetbtn);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn00);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Name = "Form1";
+            this.Text = "Noughts and Crosses";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button reset_btn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        const string CROSS = "X";
+        const string NOUGHT = "O";
+        string userMK = CROSS;
+        string CPU = NOUGHT;
+        Button[,] grid;
+        public Form1()
+        {
+            InitializeComponent();
+            grid = new Button[,] { { btn00, btn10, btn20 },
+                                   { btn01, btn11, btn21 },
+                                   { btn02 ,btn12 ,btn22 } };
+            wipe();
+        }
+
+        Button resetbtn = new Button;
+        public void resetbtn_click
+        {
+
+        }
+        
+        public void button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int x_coord = int.Parse(btn.Name[3].ToString());
+            int y_coord = int.Parse(btn.Name[4].ToString());
+            if (btn.Text == "")
+            {
+                btn.Text = userMK;  //places marker
+                lineCheck(userMK);    //check for win
+                    // switches turn
+            }
+            //MessageBox.Show(x_coord.ToString() + y_coord.ToString());
+        }
+        private void lineCheck(string mark)    //probs pass player or maker as parameter
+        {
+            bool win = false;
+            for (int i =0; i <3; i++)
+            {   //check verticle lines
+                if (grid[i,0].Text == mark & grid[i, 1].Text == mark & grid [i, 2].Text == mark)
+                {
+                    win = true;
+                }
+            }
+            for (int j = 0; j < 3; j++)
+            {   //check horrizontal lines
+                if (grid[0, j].Text == mark & grid[1, j].Text == mark & grid[2, j].Text == mark)
+                {
+                    win = true;
+                }
+            }
+            //check diagonial line for / gradient
+            if (grid[0, 0].Text == mark & grid[1, 1].Text == mark & grid[2, 2].Text == mark)
+            {
+                win = true;
+            }
+            //check diagonial line for \ gradient
+            if (grid[0, 2].Text == mark & grid[1, 1].Text == mark & grid[2, 0].Text == mark)
+            {
+                win = true;
+            }
+
+            if (win)
+            {
+                MessageBox.Show(mark + " has won");
+                wipe();
+            }
+
+
+        }
+        private void wipe()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    grid[i, j].Text = "";
+                }
+            }
+        }
+    }
+}
